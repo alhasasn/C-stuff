1258a1309b8218f623a54f02b41944e541e19c8a
alhassan <121292207+alhasasn@users.noreply.github.com>
2024-08-26 13:23:59 +0100

    Add files via upload

---
+++ isbn.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+
+    //Getting isbn
+    printf("ENTER THE ISBN :  ");
+
+    int gsi, group_identifier,publisher_code,item_number,check_digit;
+    scanf("%d-%d-%d-%d-%d",&gsi,&group_identifier,&publisher_code,&item_number,&check_digit);
+
+
+    printf("GSI PREFIX %d\n",gsi);
+    printf("group_identifier %d\n",group_identifier);
+    printf("publisher_code %d\n",publisher_code);
+    printf("item_number  %d\n",item_number);
+    printf("check_digit  %d\n",check_digit);
+
+    return 0;
+}
