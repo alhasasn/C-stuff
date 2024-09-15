8b3e4b783c62abd0c675802752c3eeb3764cfc2d
alhassan <121292207+alhasasn@users.noreply.github.com>
2024-09-15 08:54:56 +0100

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(){
+    int input_num_one,input_num_two;
+
+    printf("ENTER A NUMBER ");
+    scanf("%1d%1d",&input_num_one,&input_num_two);
+    printf("THE REVERSE OF THE NUMBER IS %d%d\n",input_num_two,input_num_one);
+
+
+    return 0;
+}
